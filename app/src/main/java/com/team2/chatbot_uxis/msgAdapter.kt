43fcfbb4b35b086efa52a5d87c5d9f3f880ef10b
bjh6654 <bjh6654@naver.com>
2020-11-12 16:21:38 +0900

    send message to chatbot

--- app/src/main/java/com/team2/chatbot_uxis/msgAdapter.kt
+++ app/src/main/java/com/team2/chatbot_uxis/msgAdapter.kt
@@ -5,39 +5,64 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.msg_box.view.*
+import kotlinx.android.synthetic.main.msg_box_answer.view.*
+import org.json.JSONException
+import org.json.JSONObject
 
-class msgAdapter(var datas:ArrayList<msgItem>):RecyclerView.Adapter<msgAdapter.ViewHolder>(){
-    class ViewHolder(view: View):RecyclerView.ViewHolder(view) {
-        var name = view.tv_name
-        //var time = view.tv_time
-        var message = view.tv_msg
-    }
+class msgAdapter(private var context: Context, var datas:ArrayList<msgItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-        lateinit var view:View
-        var context:Context = parent.context
-
-        if(viewType==0){
-            view =LayoutInflater.from(parent.context).inflate(R.layout.msg_box,parent,false)
-            return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            0 -> ViewHolderQuestion(LayoutInflater.from(context).inflate(R.layout.msg_box_question,parent,false))
+            1 -> ViewHolderAnswer(LayoutInflater.from(context).inflate(R.layout.msg_box_answer,parent,false))
+            else -> ViewHolderAnswerWithCover(LayoutInflater.from(context).inflate(R.layout.msg_box_answer,parent,false))
         }
-        else{
-            view =LayoutInflater.from(parent.context).inflate(R.layout.msg_box_bot,parent,false)
-            return ViewHolder(view)
+    }
+
+    override fun getItemCount() = datas.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolderQuestion -> {
+                holder.itemView.run {
+//                    tv_name.text = datas[position].name
+                    tv_name.text = "질문"
+                    tv_msg.text = datas[position].message
+                }
+            }
+            is ViewHolderAnswer -> {
+                holder.itemView.run {
+                    tv_name.text = "답변"
+                    tv_msg.text = datas[position].message
+                }
+            }
+            else -> {
+                holder.itemView.run {
+                    tv_name.text = "답변 with Cover"
+                    tv_msg.text = datas[position].message
+                }
+            }
         }
-//        var view = LayoutInflater.from(parent.context).inflate(R.layout.msg_box,parent,false)
-//        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return datas.size
+    override fun getItemViewType(position: Int): Int {
+//        return datas[position].viewType
+        var data = datas[position]
+        if ( data.name == "chatbot" ) {
+            try {
+                if (JSONObject(data.message).has("cover"))
+                    return 2
+            } catch (e: JSONException) {
+                return 1
+            }
+            return 1
+        } else {
+            return 0
+        }
     }
 
-    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
-        holder.name.text = datas[position].name
-        holder.message.text = datas[position].message
-    }
+    class ViewHolderQuestion(view: View):RecyclerView.ViewHolder(view)
+    class ViewHolderAnswerWithCover(view: View):RecyclerView.ViewHolder(view)
+    class ViewHolderAnswer(view: View):RecyclerView.ViewHolder(view)
 
 }
